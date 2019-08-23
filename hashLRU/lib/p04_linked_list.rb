6f4eabe9f14e9abd5f7612d4f100592585299ebb
mgoetzke <mgoetzke@gmail.com>
2019-08-23 16:37:17 -0400

    Add Linked List and Hash Map

--- hashLRU/lib/p04_linked_list.rb
+++ hashLRU/lib/p04_linked_list.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -16,47 +17,100 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+
+    # self.prev = self.next
   end
 end
 
 class LinkedList
+  include Enumerable
+  attr_reader :Sentinel, :nodes, :head, :tail
   def initialize
+    @head = Node.new
+    @tail= Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
+    ##gger
     each_with_index { |link, j| return link if i == j }
     nil
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    head_next = head.next
+    return head_next == tail 
   end
 
   def get(key)
+    search_node = head
+    until search_node == tail
+      return search_node.val if search_node.key == key
+      search_node = search_node.next
+    end
+    return nil
   end
 
   def include?(key)
+    search_node = head
+    until search_node == tail
+      return true if search_node.key == key
+      search_node = search_node.next
+    end
+    return false
   end
 
   def append(key, val)
+    before_tail = @tail.prev
+    append_node = Node.new(key, val)
+    before_tail.next = append_node
+    append_node.prev = before_tail
+    append_node.next = @tail
+    @tail.prev = append_node
   end
 
   def update(key, val)
+     if self.include?(key)
+        search_node = head
+        until search_node == tail
+          if search_node.key == key
+            search_node.val = val
+          end
+          search_node = search_node.next
+        end
+      end
   end
 
   def remove(key)
+    search_node = head
+    until search_node == tail
+      if search_node.key == key
+        search_node.prev.next = search_node.next
+        search_node.next.prev = search_node.prev
+      end
+      search_node = search_node.next
+    end
   end
 
   def each
+    search_node = first
+    until search_node == tail
+      yield search_node
+      search_node = search_node.next
+    end
   end
 
-  # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  #uncomment when you have `each` working and `Enumerable` included
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
--- hashLRU/lib/p05_hash_map.rb
+++ hashLRU/lib/p05_hash_map.rb
@@ -1,26 +1,57 @@
 require_relative 'p04_linked_list'
-
+require "byebug"
 class HashMap
   attr_accessor :count
-
+  include Enumerable
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
     @count = 0
   end
 
   def include?(key)
+    correct_bucket = bucket(key)
+    bucket_content = @store[correct_bucket]
+    bucket_content.include?(key)  
+   # @store[correct_bucket].include?(key.hash)    
+  
   end
 
   def set(key, val)
+    if self.include?(key)
+      correct_bucket = bucket(key)
+      @store[correct_bucket].update(key, val)
+    else
+      if num_buckets == count
+        # debugger
+        resize!
+      end
+      correct_bucket = bucket(key)
+      @store[correct_bucket].append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    correct_bucket = bucket(key)
+    return @store[correct_bucket].get(key)
   end
 
   def delete(key)
+    if self.include?(key)
+      correct_bucket = bucket(key)
+      @store[correct_bucket].remove(key)
+      @count -= 1
+    end
   end
 
   def each
+    #debugger
+    @store.each do | bucket |
+      #debugger
+      bucket.each do | pairs | 
+        yield pairs.key, pairs.val
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
@@ -41,9 +72,20 @@ class HashMap
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { LinkedList.new }
+    # debugger
+    self.each do |k,v|
+      # debugger
+      correct_bucket = k.hash % (num_buckets * 2)
+      new_store[correct_bucket].append(k,v)
+    end
+    @store = new_store
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    hash_key = key.hash
+    bucket = hash_key % num_buckets
+    return bucket
   end
 end
